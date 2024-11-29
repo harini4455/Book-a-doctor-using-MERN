966c1d01370c491ed8a3a56c76d95ecda2dd3f94
harini4455 <146976459+harini4455@users.noreply.github.com>
2024-11-29 11:41:03 +0530

    Create app.js

---
+++ frontend/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DoctorList from "./components/DoctorList";
+
+function App() {
+  return (
+    <div>
+      <h1>Doctor Booking System</h1>
+      <DoctorList />
+    </div>
+  );
+}
+
+export default App;
